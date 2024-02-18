cd4c5dca61ceef534c2da6ed615e7375bcfc5986
roshana01 <roshanaroshani99@gmail.com>
2024-02-18 18:44:58 +0330

    start project quiz web and added file react and initial tailwind css

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="container">
+      <h1 className="bg-pink-200 text-purple-800 text-lg container px-20">Hello my friend </h1>
+    </div>
+  );
+}
