923ccbe98858a7eacb624f5a4007a2feb584a964
roshana01 <roshanaroshani99@gmail.com>
2024-02-20 10:12:06 +0330

    finish style Quiestion

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Loading from "./Loading";
 import StartScreen from "./StartScreen";
+import Quiestion from "./Quiestion";
 
 export default function App() {
   return (
     <>
       <div className="contanier w-[100%] h-[100vh] bg-yellow-50  flex flex-col">
         {/* <Loading /> */}
-        <StartScreen />
+        {/* <StartScreen /> */}
+        <Quiestion/>
+        
       </div>
     </>
   );
